d6eb137f0b5c9cbd2963dc41baaa07dfcf78a0be
Jakob Guddas <jakob.guddas@openmailbox.org>
2020-06-01 22:51:59 +0200

    added redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import dayjs from 'dayjs'
+import { Provider } from 'react-redux'
 import {
   BrowserRouter, Switch, Route, Redirect,
 } from 'react-router-dom'
@@ -8,47 +9,50 @@ import { Error404Page } from 'tabler-react'
 import ActivitiesPage from './components/Activities/ActivitiesPage.js'
 import ActivityPage from './components/Activity/ActivityPage.js'
 
+import store from './store.js'
 import activities from './activities.js'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route
-          exact
-          path="/activities/:month"
-          render={({ match }) => (
-            <ActivitiesPage
-              activities={activities}
-              month={match.params.month}
-            />
-          )}
-        />
-        <Route
-          exact
-          path="/activity/:activityId"
-          render={({ match }) => {
-            const activity = activities.find(({ id }) => (
-              id === decodeURI(match.params.activityId)
-            ))
-            if (!activity) {
-              return (
-                <Error404Page />
-              )
-            }
-            return (
-              <ActivityPage
-                activity={activity}
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/activities/:month"
+            render={({ match }) => (
+              <ActivitiesPage
                 activities={activities}
+                month={match.params.month}
               />
-            )
-          }}
-        />
-        <Redirect
-          to={`/activities/${dayjs(activities[0].date).format('YYYY-MM')}`}
-        />
-      </Switch>
-    </BrowserRouter>
+            )}
+          />
+          <Route
+            exact
+            path="/activity/:activityId"
+            render={({ match }) => {
+              const activity = activities.find(({ id }) => (
+                id === decodeURI(match.params.activityId)
+              ))
+              if (!activity) {
+                return (
+                  <Error404Page />
+                )
+              }
+              return (
+                <ActivityPage
+                  activity={activity}
+                  activities={activities}
+                />
+              )
+            }}
+          />
+          <Redirect
+            to={`/activities/${dayjs(activities[0].date).format('YYYY-MM')}`}
+          />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
