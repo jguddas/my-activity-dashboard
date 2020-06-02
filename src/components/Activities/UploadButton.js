ee9b588e82454de27d38e160871cc6c614ed679c
Jakob Guddas <jakob.guddas@openmailbox.org>
2020-06-02 19:47:04 +0200

    removed unnecessary import in upload button component

--- src/components/Activities/UploadButton.js
+++ src/components/Activities/UploadButton.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react'
-import pLimit from 'p-limit'
 import { useDispatch } from 'react-redux'
 import { Button } from 'tabler-react'
 
