40fc948be1a235159b28238c0d422e3dce7583f2
Jakob Guddas <jakob.guddas@openmailbox.org>
2020-06-02 19:40:04 +0200

    added persisting of store

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/es/integration/react.js'
 import { BrowserRouter, Switch } from 'react-router-dom'
 
 import Routes from './Routes.js'
 
-import store from './store.js'
+import store, { persistor } from './store.js'
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <Routes />
-        </Switch>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Switch>
+            <Routes />
+          </Switch>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   )
 }
--- src/store.js
+++ src/store.js
@@ -1,6 +1,15 @@
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage/index.js'
 import logger from 'redux-logger'
 
 import rootReducer from './reducers/index.js'
 
-export default createStore(rootReducer, applyMiddleware(logger))
+const persistConfig = { key: 'root', storage }
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(persistedReducer, applyMiddleware(logger))
+
+export const persistor = persistStore(store)
+export default store
