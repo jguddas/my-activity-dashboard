c53949d91060abe7aaf3c091fef25431dfbc4856
Jakob Guddas <jakob.guddas@openmailbox.org>
2020-12-31 15:22:35 +0100

    added service worker update notification

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -66,17 +66,16 @@ function registerValidSW(swUrl, config) {
         installingWorker.onstatechange = () => {
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
-              // At this point, the updated precached content has been fetched,
-              // but the previous service worker will still serve the older
-              // content until all client tabs are closed.
-              console.log(
-                'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
-              );
+              if (!window.confirm('A new version is available!')) return
+              const waitingServiceWorker = registration.waiting
 
-              // Execute callback
-              if (config && config.onUpdate) {
-                config.onUpdate(registration);
+              if (waitingServiceWorker) {
+                waitingServiceWorker.addEventListener("statechange", event => {
+                  if (event.target.state === "activated") {
+                    window.location.reload()
+                  }
+                });
+                waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
               }
             } else {
               // At this point, everything has been precached.
