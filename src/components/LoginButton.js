30ba6a04ec1f6834efc39f10ba89a34e605525d7
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-01-06 20:56:09 +0100

    moved getCode function into separate utility file

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,30 +1,16 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { stringify as stringifyQuery } from 'query-string'
 
 import { deauthorize } from '../actions/StravaActions.js'
 
 import PageHeaderButton from './PageHeaderButton.js'
 
-import {
-  STRAVA_SCOPE,
-  STRAVA_CLIENT_ID,
-  STRAVA_OAUTH_URL,
-} from '../constants.js'
+import getCode from '../utils/getCode.js'
 
 function LoginButton() {
   const dispatch = useDispatch()
   const { athlete, accessToken } = useSelector((state) => state.Strava)
-
-  const getCode = () => {
-    window.open(`${STRAVA_OAUTH_URL}?${stringifyQuery({
-      client_id: STRAVA_CLIENT_ID,
-      response_type: 'code',
-      redirect_uri: `${window.location.origin}/exchange-token`,
-      scope: STRAVA_SCOPE,
-    })}`, '_self')
-  }
 
   const unAuth = () => dispatch(deauthorize(accessToken))
 
