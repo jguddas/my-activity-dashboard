d55976af9bfc403412c38a1087ce145123fe3678
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-01-06 21:47:50 +0100

    added lazy loading of activity map in activity map with slider component

--- src/components/ActivityMapWithSlider.js
+++ src/components/ActivityMapWithSlider.js
@@ -6,7 +6,10 @@ import { Card, Button, Form, colors } from 'tabler-react'
 
 import formatDuration from '../utils/formatDuration.js'
 
-import ActivityMap from './ActivityMap.js'
+const ActivityMap = React.lazy(() => import(
+  /* webpackChunkName: "ActivityMap" */
+  './ActivityMap.js'
+))
 
 function ActivityMapWithSlider({ activity, matchedActivities, factor = 0.005 }) {
   const maxActivityDurationInMinutes = useMemo(() => Math.ceil(
@@ -45,64 +48,78 @@ function ActivityMapWithSlider({ activity, matchedActivities, factor = 0.005 }) 
 
   const [isFullscreen, setFullscreen] = useState(screenfull.isFullscreen)
 
+  const height = isFullscreen ? 'calc(100vh - 87px)' : 350
+
+  const body = (
+    <MyCardBody className="bg-white">
+      <div className="d-flex">
+        <Button
+          icon={playing ? 'pause' : 'play'}
+          prefix="fe"
+          color="purple"
+          className="mr-4"
+          onClick={() => setState((matchedState) => ({
+            playing: !matchedState.playing,
+            time: matchedState.time % (maxActivityDurationInMinutes),
+          }))}
+        />
+        <MyRangeSlider
+          type="range"
+          className="form-control custom-range"
+          onChange={(e) => setState(({ time: Number(e.target.value) }))}
+          step={0.1}
+          min={0}
+          max={maxActivityDurationInMinutes}
+          value={round(time, 2)}
+        />
+        <Form.MaskedInput
+          className="w-9 ml-4 form-control text-center"
+          onChange={(e) => {
+            const [hours, minutes, seconds] = e.target.value.split(':')
+            const nexttime = (
+              parseInt(hours, 10) * 60
+              + parseInt(minutes, 10)
+              + parseInt(seconds, 10) / 60
+            )
+            if (nexttime <= maxActivityDurationInMinutes) {
+              setState({ time: nexttime })
+            }
+          }}
+          onClick={() => setState((matchedState) => ({
+            time: matchedState.time,
+            playing: false,
+          }))}
+          mask={[/\d/, /\d/, ':', /\d/, /\d/, ':', /\d/, /\d/]}
+          value={formatDuration(time * 60000)}
+        />
+      </div>
+    </MyCardBody>
+  )
+
   return (
     <div className="card">
-      <ActivityMap
-        key={activity.id}
-        activity={activity}
-        matchedActivities={matchedActivities}
-        trimEnd={time * 60000}
-        controls
-        scrollWheelZoom={isFullscreen}
-        dragging={isFullscreen || 'touch'}
-        setFullscreen={setFullscreen}
-        smoothFactor={3}
-        height={isFullscreen ? 'calc(100vh - 87px)' : 350}
+      <React.Suspense fallback={(
+        <>
+          <div style={{ height }} />
+          {body}
+        </>
+)}
       >
-        <MyCardBody className="bg-white">
-          <div className="d-flex">
-            <Button
-              icon={playing ? 'pause' : 'play'}
-              prefix="fe"
-              color="purple"
-              className="mr-4"
-              onClick={() => setState((matchedState) => ({
-                playing: !matchedState.playing,
-                time: matchedState.time % (maxActivityDurationInMinutes),
-              }))}
-            />
-            <MyRangeSlider
-              type="range"
-              className="form-control custom-range"
-              onChange={(e) => setState(({ time: Number(e.target.value) }))}
-              step={0.1}
-              min={0}
-              max={maxActivityDurationInMinutes}
-              value={round(time, 2)}
-            />
-            <Form.MaskedInput
-              className="w-9 ml-4 form-control text-center"
-              onChange={(e) => {
-                const [hours, minutes, seconds] = e.target.value.split(':')
-                const nexttime = (
-                  parseInt(hours, 10) * 60
-                  + parseInt(minutes, 10)
-                  + parseInt(seconds, 10) / 60
-                )
-                if (nexttime <= maxActivityDurationInMinutes) {
-                  setState({ time: nexttime })
-                }
-              }}
-              onClick={() => setState((matchedState) => ({
-                time: matchedState.time,
-                playing: false,
-              }))}
-              mask={[/\d/, /\d/, ':', /\d/, /\d/, ':', /\d/, /\d/]}
-              value={formatDuration(time * 60000)}
-            />
-          </div>
-        </MyCardBody>
-      </ActivityMap>
+        <ActivityMap
+          key={activity.id}
+          activity={activity}
+          matchedActivities={matchedActivities}
+          trimEnd={time * 60000}
+          controls
+          scrollWheelZoom={isFullscreen}
+          dragging={isFullscreen || 'touch'}
+          setFullscreen={setFullscreen}
+          smoothFactor={3}
+          height={height}
+        >
+          {body}
+        </ActivityMap>
+      </React.Suspense>
     </div>
   )
 }
