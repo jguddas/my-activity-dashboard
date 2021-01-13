7bfd335867672f33ee3f293b35a1df2c664e68f4
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-01-13 21:31:41 +0100

    removed tabler-react dependency in splits table

--- src/components/SplitsTable.js
+++ src/components/SplitsTable.js
@@ -1,24 +1,25 @@
 import React from 'react'
-import { Table } from 'tabler-react'
 import { Link } from 'react-router-dom'
 
 const SplitsTable = ({ splits }) => (
-  <Table cards striped responsive>
-    <Table.Body>
-      {splits.map(({ id, name }) => (
-        <Table.Row
-          className="d-block d-md-table-row"
-          key={id}
-        >
-          <Table.Col>
-            <Link to={`/split/${id}`}>
-              {name}
-            </Link>
-          </Table.Col>
-        </Table.Row>
-      ))}
-    </Table.Body>
-  </Table>
+  <div className="table-responsive">
+    <table className="table card-table table-striped">
+      <tbody>
+        {splits.map(({ id, name }) => (
+          <tr
+            className="d-block d-md-table-row"
+            key={id}
+          >
+            <td>
+              <Link to={`/split/${id}`}>
+                {name}
+              </Link>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
 )
 
 export default SplitsTable
