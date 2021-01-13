88913fce96c937e0bcfbde1995b76f52de01bc20
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-01-13 21:48:36 +0100

    removed tabler-react dependency in error 404 page component

--- src/components/Error404Page.js
+++ src/components/Error404Page.js
@@ -1,3 +1,24 @@
-import { Error404Page } from 'tabler-react'
+import React from 'react'
+import { withRouter } from 'react-router-dom'
 
-export default Error404Page
+const Error404Page = ({ history }) => (
+  <div className="page text-center">
+    <div className="container">
+      <h1 className="h1 mt-0 mb-4 display-1 text-muted mb-5">404</h1>
+      <h2 className="h2 mt-0 mb-4">Oops... You just found an error page...</h2>
+      <h4 className="h4 mt-0 mb-4 text-muted font-weight-normal mb-7">
+        We are sorry but the page you have requested can not be found...
+      </h4>
+      <button
+        className="btn btn-purple"
+        type="button"
+        onClick={() => history.goBack()}
+      >
+        <i className="fe fe-arrow-left mr-2" />
+        Go back
+      </button>
+    </div>
+  </div>
+)
+
+export default withRouter(Error404Page)
