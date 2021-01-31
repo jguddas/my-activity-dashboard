2c50d1cf3ac0bb67157516c67dc08e8eab7007ba
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-01-31 22:09:39 +0100

    added typings to fetch with query utility

--- src/utils/fetchWithQuery.ts
+++ src/utils/fetchWithQuery.ts
@@ -4,11 +4,19 @@ import { stringify as stringifyQuery } from 'query-string'
 
 const snakeCaseKeys = (obj) => mapKeys(obj, (val, key) => snakeCase(key))
 
-const fetchWithQuery = (url, { query, ...opts }) => fetch(
-  `${url}${query ? '?' : ''}${stringifyQuery(snakeCaseKeys(query))}`,
-  opts,
-).then((res) => (
-  res.json().then((json) => (res.ok ? json : Promise.reject(json)))
-))
+export interface RequestInitWithQuery extends RequestInit {
+  query: {}
+}
+
+function fetchWithQuery<T>(url: string, { query, ...opts }: RequestInitWithQuery): Promise<T> {
+  return (
+    fetch(
+      `${url}${query ? '?' : ''}${stringifyQuery(snakeCaseKeys(query))}`,
+      opts,
+    ).then((res) => (
+      res.json().then((json) => (res.ok ? json : Promise.reject(json)))
+    ))
+  )
+}
 
 export default fetchWithQuery
