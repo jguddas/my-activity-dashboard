90564cd0d9d257f153e08464808a337a049e87a5
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-02-09 19:59:27 +0100

    added typings to split reducer

--- src/reducers/SplitReducer.ts
+++ src/reducers/SplitReducer.ts
@@ -3,16 +3,23 @@ import unionBy from 'lodash/unionBy'
 
 import { addSplit } from '../actions/SplitActions'
 
-const defaultSplits = [5, 10, 20, 30, 50, 80, 100].map((distance) => ({
+import { StoredSplit, DistanceSplit } from '../types/split'
+
+const defaultSplits = [5, 10, 20, 30, 50, 80, 100].map((distance):DistanceSplit => ({
   name: `Default ${distance}km Split`,
   id: `default-distance-${distance}`,
   type: 'distance',
   distance,
 }))
 
-export default createReducer({ splits: defaultSplits }, {
-  [addSplit]: (state, action) => ({
-    ...state,
-    splits: unionBy([action.payload], state.splits, 'id'),
-  }),
-})
+type State = { splits: StoredSplit[] }
+
+const initialState = { splits: defaultSplits } as State
+
+export default createReducer(initialState, (builder) => (
+  builder
+    .addCase(addSplit, (state, action):State => ({
+      ...state,
+      splits: unionBy([action.payload], state.splits, 'id'),
+    }))
+))
