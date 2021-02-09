897681efe478857b2ebc132e0eb64669f2047709
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-02-09 20:03:16 +0100

    added typings to format duration utilty

--- src/utils/formatDuration.ts
+++ src/utils/formatDuration.ts
@@ -1,6 +1,6 @@
 const padZero = (x) => (parseInt(x, 10) < 10 ? `0${x}` : `${x}`)
 
-const formatDuration = (duration) => `${
+const formatDuration = (duration:number):string => `${
   padZero(Math.floor(duration / 3600000) % 60)
 }:${
   padZero(Math.floor(duration / 60000) % 60)
