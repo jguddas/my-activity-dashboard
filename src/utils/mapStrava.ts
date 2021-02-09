dea8de9940494f08b686227fed76aa884832b245
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-02-09 20:03:33 +0100

    added typings to map strava utilty

--- src/utils/mapStrava.ts
+++ src/utils/mapStrava.ts
@@ -1,10 +1,32 @@
 /* eslint-disable camelcase */
 import dayjs from 'dayjs'
-import last from 'lodash/last'
 import round from 'lodash/round'
 
-const mapStrava = ({ id, name, distance, streams, start_date, elapsed_time }) => {
-  const trkpts = streams ? streams.time.data.map((time, idx) => ([
+import { Activity, Trkpt } from '../types/activity'
+
+type Arg = {
+  id: string
+  name: string
+  distance: number
+  streams: {
+    time:{data:number[]}
+    latlng:{data:[number, number][]}
+    altitude:{data:number[]}
+    distance:{data:number[]}
+  }
+  start_date: string
+  elapsed_time: number
+}
+
+const mapStrava = ({
+  id,
+  name,
+  distance,
+  streams,
+  start_date,
+  elapsed_time,
+}:Arg):Activity => {
+  const trkpts = streams ? streams.time.data.map((time, idx):Trkpt => ([
     streams.latlng.data[idx][0],
     streams.latlng.data[idx][1],
     streams.altitude.data[idx],
@@ -17,16 +39,22 @@ const mapStrava = ({ id, name, distance, streams, start_date, elapsed_time }) =>
   return {
     id: `${id}`,
     externalLink: `https://www.strava.com/activities/${id}`,
-    endTime: startTime.add(elapsed_time, 'seconds').toISOString(),
+    endTime: startTime.add(elapsed_time, 'second').toISOString(),
     startTime: startTime.toISOString(),
     distance: distance / 1000,
     name,
     duration: elapsed_time * 1000,
     date: startTime.format('YYYY-MM-DD'),
-    trkpts,
-    startpt: trkpts?.[0],
-    endpt: trkpts ? last(trkpts) : null,
     speed: distance / (elapsed_time / 3.6),
+    ...(trkpts && trkpts[0] ? {
+      trkpts,
+      startpt: trkpts[0],
+      endpt: trkpts[trkpts.length - 1],
+    } : {
+      trkpts: null,
+      startpt: null,
+      endpt: null,
+    }),
   }
 }
 
