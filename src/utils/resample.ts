7f9f6c7015e07e7048c5ffe6c1a1162a75a2ba39
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-02-09 20:03:33 +0100

    added typings to resample utilty

--- src/utils/resample.ts
+++ src/utils/resample.ts
@@ -1,8 +1,12 @@
-const lerp = ([x0, y0], [x1, y1], i) => y0 + ((y1 - y0) / (x1 - x0)) * (i - x0)
+const lerp = (
+  [x0, y0]:[number, number],
+  [x1, y1]:[number, number],
+  i:number,
+) => y0 + ((y1 - y0) / (x1 - x0)) * (i - x0)
 
-const resample = (inp, scale) => {
+const resample = (inp:[number, number][], scale:number) => {
   let arr = [...inp]
-  const out = []
+  const out:[number, number][] = []
   const max = arr[arr.length - 1][0]
   for (let i = 0; i < max; i += scale) {
     const idx = arr.findIndex(([x]) => x >= i)
