fffb7ac9d16bb13f52574c87ba6bf3f8ef22b807
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-02-09 19:10:23 +0100

    improved activity typings

--- src/types/activity.ts
+++ src/types/activity.ts
@@ -1,22 +1,39 @@
-export type Trkpt = [
-  number,
-  number,
-  number,
-  number,
-  number,
-]
+export type Trkpt = [ number, number, number, number, number ]
 
-export type Activity = {
+export type ActivityWithTrkpts = {
   id: string
   externalLink: string
-  endTime: Date
-  startTime: Date
+  endTime: string
+  startTime: string
   distance: number
   name: string
   duration: number
-  date: Date
+  date: string
   trkpts: Trkpt[]
   startpt: Trkpt
   endpt: Trkpt
   speed: number
+}
+
+export type ActivityWithoutTrkpts = {
+  id: string
+  externalLink: string
+  endTime: string
+  startTime: string
+  distance: number
+  name: string
+  duration: number
+  date: string
+  trkpts: null
+  startpt: null
+  endpt: null
+  speed: number
+}
+
+export type Activity = ActivityWithTrkpts | ActivityWithoutTrkpts
+
+export type SkeletonActivity = {
+  duration: number
+  trkpts: [number, number, number, number][]
+  endpt: Trkpt
 }
