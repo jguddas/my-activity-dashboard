3d46a6aad3c807c9338a70db36b4eb5c0b5ad8f6
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-02-09 20:07:33 +0100

    added typings to back button component

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { withRouter } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 import PageHeaderButton from './PageHeaderButton'
 
-const BackButton = ({ history }) => (
-  <PageHeaderButton
-    onClick={() => history.goBack()}
-    className=""
-    icon="arrow-left"
-  >
-    Back
-  </PageHeaderButton>
-)
+const BackButton = ():JSX.Element => {
+  const history = useHistory()
+  return (
+    <PageHeaderButton
+      onClick={() => history.goBack()}
+      className=""
+      icon="arrow-left"
+    >
+      Back
+    </PageHeaderButton>
+  )
+}
 
-export default withRouter(BackButton)
+export default BackButton
