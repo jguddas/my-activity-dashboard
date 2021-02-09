2ce27de720e11dc5d4f13194d85aa1c5ba4a6dd3
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-02-09 19:59:48 +0100

    added typings to strava reducer

--- src/reducers/StravaReducer.ts
+++ src/reducers/StravaReducer.ts
@@ -5,20 +5,49 @@ import {
   deauthorize,
 } from '../actions/StravaActions'
 
-export default createReducer({}, {
-  [exchangeToken.fulfilled]: (state, action) => ({
-    ...state,
-    accessToken: action.payload.access_token,
-    accessTokenExpiresAt: action.payload.expires_at,
-    refreshToken: action.payload.refresh_token,
-    athlete: action.payload.athlete,
-  }),
-  [refreshAuth.rejected]: () => ({}),
-  [refreshAuth.fulfilled]: (state, action) => ({
-    ...state,
-    accessToken: action.payload.access_token,
-    accessTokenExpiresAt: action.payload.expires_at,
-    refreshToken: action.payload.refresh_token,
-  }),
-  [deauthorize.pending]: () => ({}),
-})
+type State = {
+  accessToken?: string
+  accessTokenExpiresAt?: number
+  refreshToken?: string
+  athlete?: {
+    id: number
+    username: null | string
+    resource_state: number
+    firstname: string
+    lastname: string
+    city: string
+    state: string
+    country: string
+    sex: string
+    premium: boolean
+    summit: boolean
+    created_at: string
+    updated_at: string
+    badge_type_id: number
+    profile_medium: string
+    profile: string
+    friend: null
+    follower: null
+  }
+}
+
+const initialState = {} as State
+
+export default createReducer(initialState, (builder) => (
+  builder
+    .addCase(exchangeToken.fulfilled, (state, action):State => ({
+      ...state,
+      accessToken: action.payload.access_token,
+      accessTokenExpiresAt: action.payload.expires_at,
+      refreshToken: action.payload.refresh_token,
+      athlete: action.payload.athlete,
+    }))
+    .addCase(exchangeToken.rejected, ():State => ({}))
+    .addCase(refreshAuth.fulfilled, (state, action):State => ({
+      ...state,
+      accessToken: action.payload.access_token,
+      accessTokenExpiresAt: action.payload.expires_at,
+      refreshToken: action.payload.refresh_token,
+    }))
+    .addCase(deauthorize.pending, ():State => ({}))
+))
