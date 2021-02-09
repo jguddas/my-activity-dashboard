4691693586bc834bd8cc08b41ee6428ad05e06e1
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-02-09 20:07:33 +0100

    added typings to scroll to top on location change component

--- src/components/ScrollToTopOnLocationChange.tsx
+++ src/components/ScrollToTopOnLocationChange.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
-function ScrollToTopOnLocationChange() {
+function ScrollToTopOnLocationChange():null {
   const { pathname } = useLocation()
 
   useEffect(() => {
