a1d7fa90a51284bb26e3029e03fc5877d8fb1460
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-02-14 16:36:38 +0100

    improved fetch with query utility

--- src/utils/fetchWithQuery.ts
+++ src/utils/fetchWithQuery.ts
@@ -1,17 +1,29 @@
 import snakeCase from 'lodash/snakeCase'
 import mapKeys from 'lodash/mapKeys'
+import mapValues from 'lodash/mapValues'
 import { stringify as stringifyQuery } from 'query-string'
 
-const snakeCaseKeys = (obj:{[key:string]: any}) => mapKeys(obj, (_, key) => snakeCase(key))
+type QueryValue = string | number | boolean
+
+export type Query = { [key: string]: QueryValue | QueryValue[] }
 
 export interface RequestInitWithQuery extends RequestInit {
-  query: { [key: string]: string }
+  query: Query
 }
 
+const snakeCaseKeys = (obj:{[key:string]: QueryValue}) => mapKeys(obj, (_, key) => snakeCase(key))
+
 function fetchWithQuery<T>(url: string, { query, ...opts }: RequestInitWithQuery): Promise<T> {
+  const flattenedQuery = mapValues(query, (val) => (
+    Array.isArray(val)
+      ? val
+        .map((x) => snakeCase(x.toString()))
+        .join(',')
+      : val
+  ))
   return (
     fetch(
-      `${url}${query ? '?' : ''}${stringifyQuery(snakeCaseKeys(query))}`,
+      `${url}${query ? '?' : ''}${stringifyQuery(snakeCaseKeys(flattenedQuery))}`,
       opts,
     ).then((res) => (
       res.json().then((json:T) => (res.ok ? json : Promise.reject(json)))
