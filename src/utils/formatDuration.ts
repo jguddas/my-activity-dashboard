32cd6feec8628053e329d570f88a50ec2742f9d2
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-04-30 20:30:15 +0200

    fix: fixed issue in format duration utility

--- src/utils/formatDuration.ts
+++ src/utils/formatDuration.ts
@@ -1,7 +1,7 @@
 const padZero = (x:number) => (x < 10 ? `0${x}` : `${x}`)
 
 const formatDuration = (duration:number):string => `${
-  padZero(Math.floor(duration / 3600000) % 60)
+  padZero(Math.floor(duration / 3600000))
 }:${
   padZero(Math.floor(duration / 60000) % 60)
 }:${
