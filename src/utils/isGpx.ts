5b41c0edcd7a6835fd65916ffbf447073d6ee22b
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-04-30 20:35:19 +0200

    feat: improved validation in is gpx utility

--- src/utils/isGpx.ts
+++ src/utils/isGpx.ts
@@ -1,4 +1,11 @@
 import { object, string, array } from 'ts-blaze'
+import isFinite from 'lodash/isFinite'
+
+const DATE_TIME_REGEXP = /^(\d\d\d\d)-(\d\d)-(\d\d)(t|\s)(\d\d):(\d\d):(\d\d)(\.\d+)?(z|[+-]\d\d:\d\d)?$/i
+
+export const isDateTime = DATE_TIME_REGEXP.test.bind(DATE_TIME_REGEXP)
+
+export const isNumberString = (s: string):boolean => isFinite(parseFloat(s))
 
 const isGpx = object({
   gpx: object({
@@ -6,11 +13,11 @@ const isGpx = object({
       name: object({ _text: string() }),
       trkseg: object({
         trkpt: array(object({
-          time: object({ _text: string() }),
-          ele: object({ _text: string() }),
+          time: object({ _text: string().satisfies(isDateTime) }),
+          ele: object({ _text: string().satisfies(isNumberString) }),
           _attributes: object({
-            lat: string(),
-            lon: string(),
+            lat: string().satisfies(isNumberString),
+            lon: string().satisfies(isNumberString),
           }),
         })).satisfies((val) => val.length > 1),
       }),
