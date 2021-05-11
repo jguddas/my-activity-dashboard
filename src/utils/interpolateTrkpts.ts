f4da6c2844774b5299371d726d86b24dec7b108c
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-05-11 20:33:44 +0200

    chore: added typings to interpolate trkpts utility

--- src/utils/interpolateTrkpts.ts
+++ src/utils/interpolateTrkpts.ts
@@ -1,4 +1,6 @@
-const interpolatePosition = ([lat1, lon1], [lat2, lon2], duration, t) => {
+import { Trkpt } from '../types/activity'
+
+const interpolatePosition = ([lat1, lon1]:Trkpt, [lat2, lon2]:Trkpt, duration:number, t:number) => {
   let k = t / duration
   k = (k > 0) ? k : 0
   k = (k > 1) ? 1 : k
@@ -7,17 +9,19 @@ const interpolatePosition = ([lat1, lon1], [lat2, lon2], duration, t) => {
     lon1 + k * (lon2 - lon1),
   ]
 }
-const interpolatePositions = (pt1, pt2) => {
+const interpolatePositions = (pt1: Trkpt, pt2: Trkpt):Trkpt[] => {
   const duration = (pt2[3] - pt1[3])
-  const points = []
+  const points:Trkpt[] = []
   for (let i = 1000; i <= duration; i += 1000) {
-    points.push([...interpolatePosition(pt1, pt2, duration, i), 0, pt1[3] + i])
+    const [lat, lon] = interpolatePosition(pt1, pt2, duration, i)
+    points.push([lat, lon, 0, pt1[3] + i, 0] as Trkpt)
   }
   return points
 }
 
-const interpolateTrkpts = (trkpts) => trkpts.flatMap((trkpt, idx) => (
-  idx ? interpolatePositions(trkpts[idx - 1], trkpt) : [trkpt]
-))
+const interpolateTrkpts = (trkpts:Trkpt[]):Trkpt[] => (
+  trkpts.flatMap((trkpt, idx) => (
+    idx ? interpolatePositions(trkpts[idx - 1], trkpt) : [trkpt]
+  )))
 
 export default interpolateTrkpts
