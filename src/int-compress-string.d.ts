7b8b5f0b381f2ec977790bdf49390d3864e13196
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-05-11 20:53:09 +0200

    chore: added declaration files for int-compress-string and polyline-encode

---
+++ src/int-compress-string.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars,import/prefer-default-export */
+declare module 'int-compress-string' {
+  export const compress = (arr: number[]):string => {}
+  export const decompress = (str: string):number[] => {}
+}
---
+++ src/polyline-encoded.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars,import/prefer-default-export */
+declare module 'polyline-encoded' {
+  export const encode = (arr: number[][]):string => {}
+  export const decode = (str: string):number[][] => {}
+}
