9d6d2fac91881e8aad4332680f904b8b9fc99ba0
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-05-12 18:10:36 +0200

    feat: changed service worker update dialog to new alert

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import './index.scss'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
+import alert from './utils/alert'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,4 +16,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+serviceWorker.register({ alert })
